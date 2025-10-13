18a51bbc0a0fccddbabd457276ed6201991ecbd4
Petr Lorenc <lorenc55petr@seznam.cz>
2025-10-13 07:32:52 +0200

    FIX: update of 02

---
+++ 02/solution_get_almost_safe_patterns.py
@@ -0,0 +1,42 @@
+
+from generic_tester import GenericSolution
+
+
+class Solution(GenericSolution):
+    """
+    Calculate the number of almost safe patterns in the input data.
+    A pattern is considered almost safe if it can be made safe by removing at most one number.
+    """
+
+    def __init__(self):
+        self.all = 0
+        self.safe = 0
+
+    def check_if_correct(self, l):
+        pairs = [x - y for x, y in zip(l, l[1:])]
+        if all([0 < x <= 3 for x in pairs]):
+            return True
+        if all([-3 <= x < 0 for x in pairs]):
+            return True
+        return False
+
+
+    def solution(self, input_data: str):
+        for line in input_data.splitlines(keepends=False):
+            self.all += 1
+            split_line = [int(x) for x in line.strip().split(" ")]
+            if self.check_if_correct(split_line):
+                self.safe += 1
+            else:
+                for idx in range(len(split_line)):
+                    if idx == 0:
+                        new_split_line = split_line[1:]
+                    elif idx == len(split_line) - 1:
+                        new_split_line = split_line[:-1]
+                    else:
+                        new_split_line = split_line[0:idx] + split_line[idx + 1:]
+                    if self.check_if_correct(new_split_line):
+                        self.safe += 1
+                        break
+        return self.all, self.safe
+
---
+++ 02/solution_get_safe_patterns.py
@@ -0,0 +1,38 @@
+
+from generic_tester import GenericSolution
+
+
+class Solution(GenericSolution):
+    """
+    Calculate the number of safe patterns in the input data. A pattern is considered safe if the differences between consecutive numbers
+    are all positive and less than or equal to 3, or all negative and greater than or equal to -3.
+    """
+    def __init__(self):
+        self.all = 0
+        self.safe = 0
+
+
+    def solution(self, input_data: str):
+        for line in input_data.splitlines(keepends=False):
+            self.all += 1
+            split_line = [int(x) for x in line.strip().split(" ")]
+            first_num = split_line[0]
+            last_num = split_line[1]
+
+            if first_num == last_num or abs(last_num - first_num) > 3:
+                continue
+            increasing = first_num < last_num
+            is_safe = True
+            for char in split_line[2:]:
+                if increasing and last_num < int(char) and abs(last_num - int(char)) <= 3:
+                    pass
+                elif not increasing and last_num > int(char) and abs(last_num - int(char)) <= 3:
+                    pass
+                else:
+                    is_safe = False
+                last_num = int(char)
+            if is_safe:
+                self.safe += 1
+        return self.all, self.safe
+
+
