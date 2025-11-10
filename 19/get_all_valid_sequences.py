9fdbd0c515c984d9573da2ac8836f2f39fd7c5ac
Petr Lorenc <petr.lorenc.ai@gmail.com>
2025-11-10 08:32:47 +0100

    FEAT: adding lru

--- 19/get_all_valid_sequences.py
+++ 19/get_all_valid_sequences.py
@@ -1,37 +1,38 @@
 from functools import lru_cache
-
-with open("input_2.txt") as f:
-    input_data = f.readlines()
-
-options = input_data[0]
-designs = input_data[2:]
-
-option_mapping = {}
-
-for option in options.split(","):
-    option = option.strip()
-    option_mapping.setdefault(option[0], set()).add(option)
-
-print(option_mapping)
-designs = map(str.strip, designs)
+from generic_tester import GenericSolution
 
 
-@lru_cache(maxsize=None)
-def try_pattern(design: str):
-    if design == "":
-        return 1
-    response = 0
-    for pattern in option_mapping.get(design[0], []):
-        if design.startswith(pattern):
-            response += try_pattern(design[len(pattern) :])
-    return response
+class Solution(GenericSolution):
+    def solution(self, input_data: str):
+        """
+        This solution uses a recursive approach to check if each design can be formed
+        using the given set of valid option patterns. It builds a mapping of starting
+        characters to their corresponding patterns for efficient lookup.
+        """
 
+        input_lines = input_data.strip().split("\n")
+        options = input_lines[0]
+        designs = input_lines[2:]
 
-# print(sum([1 for design in designs if try_pattern(design, option_mapping)]))
-cnt = 0
-for design in designs:
-    response = try_pattern(design)
-    print(design, response)
-    if response:
-        cnt += response
-    print(cnt)
+        option_mapping = {}
+
+        for option in options.split(","):
+            option = option.strip()
+            option_mapping.setdefault(option[0], set()).add(option)
+
+        designs = map(str.strip, designs)
+
+        @lru_cache(maxsize=None)
+        def try_pattern(design: str) -> int:
+            if design == "":
+                return 1
+            response = 0
+            for pattern in option_mapping.get(design[0], []):
+                if design.startswith(pattern):
+                    response += try_pattern(design[len(pattern) :])
+                if response:
+                    return response
+            return response
+
+        valid_count = sum(1 for design in designs if try_pattern(design))
+        return str(valid_count)
